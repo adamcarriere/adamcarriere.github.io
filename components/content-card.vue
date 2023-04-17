a5946284a87b28fb2fd29bae6c206f6c27950249
Adam Carriere <carriere.ae@gmail.com>
2023-04-16 21:18:14 -0400

    Adjusted the styles for mobile devices.

--- components/content-card.vue
+++ components/content-card.vue
@@ -9,15 +9,18 @@
 
 .content-card {
     margin: auto;
+    background-color: white;
+    max-width: 1280px;
     padding: vars.$gutter;
     border: vars.$gutter/4 solid black;
-    background-color: white;
-    filter: drop-shadow(vars.$gutter vars.$gutter);
-    width: 80vw;
-    max-width: 1280px;
 
     a {
         color: inherit;
+    }
+
+    @media screen and (min-width: 801px) {
+        width: 80vw;
+        filter: drop-shadow(vars.$gutter vars.$gutter);
     }
 }
 </style>
